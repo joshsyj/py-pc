e3676952e2ece1cf3bda794a046737c85c0f8908
shiyingjie <shiyingjie@rr.tv>
2022-08-14 22:22:46 +0800

    init

---
+++ pc.py
@@ -0,0 +1,47 @@
+
+import re
+import requests
+
+lis = []
+lis_1 = []
+
+for page in range(70, 100):
+    url = f'https://free.kuaidaili.com/free/inha/{page}/'
+    print(url)
+    response = requests.get(url)
+
+   
+    # print(response.text)
+
+    ip_list = re.findall('<td data-title="IP">(.*?)</td>',response.text)
+    port_list = re.findall('<td data-title="PORT">(.*?)</td>',response.text)
+
+    # print(ip_list)
+    # print(port_list)
+
+    for ip,port in zip(ip_list,port_list):
+        
+        proxy = ip+':'+port
+        proxies_dict = {
+            'http':'http://'+proxy,
+            'https':'https://'+proxy
+        }
+        
+        # print(proxies_dict)
+        lis.append(proxies_dict)
+        try:
+            response = requests.get(url=url,proxies=proxies_dict,timeout=1)
+            if response.status.code==200:
+                print('当前代理ip'+proxies_dict,'可以使用')
+                lis_1.append(proxies_dict)
+                print('获取可用的ip代理数量',len(lis_1))  
+                print('获取可用的ip代理数量',lis_1)  
+                
+                
+        except:
+            print('当前代理ip请求超时')
+   
+print('获取ip代理数量',len(lis))
+print('获取可用的ip代理数量',len(lis_1))  
+print('获取可用的ip代理数量',lis_1)  
+  
